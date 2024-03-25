aaae8eafd393323cd9948aa2d1a5068f0d1fa0d0
Mohamed-art-lab <mohamed.farah@student.moringaschool.com>
2024-03-25 04:51:59 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        return [word for word in possible_anagrams if self.is_anagram(word)]
+
+    def is_anagram(self, other_word):
+        # Check if two words are anagrams
+        return sorted(self.word.lower()) == sorted(other_word.lower())
